c25789a7010cb9032fc1fb5645382631d994f6a1
mahdi83777 <mye09@mail.aub.edu>
2023-08-02 02:20:32 +0300

    updates

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -12,6 +12,7 @@ import './style.css';
 const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
+  
   const dispatch = useDispatch();
   const state = useSelector((state) => state);
   const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
